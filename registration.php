516f10d5a854544fe4859b9fdd2939c8868fd23f
X19392146 <x19392146@student.ncirl.ie>
2021-04-26 19:32:37 +0100

    updates to validation and registration pages

--- registration.php
+++ registration.php
@@ -3,9 +3,9 @@
 session_start();
 
 
-$con = mysqli_connect('localhost', 'root', '');
+$con = mysqli_connect('eu-cdbr-west-01.cleardb.com', 'b2680a39c3e361', 'b403c2b9');
 
-mysqli_select_db($con, 'registration');
+mysqli_select_db($con, 'heroku_e32aaf0c929f888');
 
 $name = $_POST['email'];
 $pass = $_POST['password'];
