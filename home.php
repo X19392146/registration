4e9a2cc888c0344485f970d33e2ccf0223f00b8a
X19392146 <x19392146@student.ncirl.ie>
2021-04-27 20:27:14 +0100

    update to links

--- home.php
+++ home.php
@@ -38,6 +38,11 @@ if(!isset($_SESSION['email'])){
 	<h2>Welcome <?php echo $_SESSION['email']; ?></h2>
 	
 	<div class="container">
+	
+	<div class="center">
+	<a href="logout.php"><button type="button" class="logoutbtn"> WRITE HERE</button></a>
+	</div>
+	
 	<div class="center">
 	<a href="logout.php"><button type="button" class="logoutbtn"> LOG OUT</button></a>
 	</div>
